1d7a33f5503cd6151f2d92765c7495bae5bb8b82
Yann Planchais <yann.planchais@randstaddigital.fr>
2024-10-28 15:24:53 +0100

    initial repo

---
+++ src/main/java/fr/yopsolo/formation/hibernate4All/Hibernate4AllApplication.java
@@ -0,0 +1,13 @@
+package fr.yopsolo.formation.hibernate4All;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate4AllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate4AllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/yopsolo/formation/hibernate4All/Hibernate4AllApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.yopsolo.formation.hibernate4All;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate4AllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
